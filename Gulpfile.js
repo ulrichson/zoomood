9140839642b175af378facc339907a29be94d6c8
Jonatan Dahl <jo_da12@hotmail.com>
2014-06-11 02:57:52 +0200

    gulp, less, livereload

---
+++ Gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// plugins
+var gulp  = require('gulp'),
+    cssmin = require('gulp-cssmin'),
+    less = require('gulp-less'),
+    server = require('gulp-livereload'),
+    autoprefixer = require('gulp-autoprefixer');
+
+// other vars
+var cssRoot = 'public/css/';
+
+gulp.task('less', function() {
+  gulp.src(cssRoot + 'less/style.less')
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(cssRoot));
+})
+
+gulp.task('build', ['less'], function() {
+  gulp.src(cssRoot + 'style.css') // DOES NOT WORK.........................
+    .pipe(cssmin({keepSpecialComments:0}))
+    .pipe(gulp.dest(cssRoot));
+});
+
+gulp.task('default', function () {
+  server.listen();
+  gulp.watch('public/css/less/**/*.less', ['less']);
+  gulp.watch('public/css/style.css').on('change', function(file) {
+    server.changed(file.path);
+  });
+});
