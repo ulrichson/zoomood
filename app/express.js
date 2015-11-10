f465c886ac9119688b92f0743cb5b545e264d05e
Ulrich Lehner <me@ulrichlehner.com>
2015-11-10 12:39:21 +0100

    Fixed 404 error for favicon request

--- app/express.js
+++ app/express.js
@@ -8,7 +8,7 @@ module.exports = function(app, config) {
   app.set('port', process.env.PORT || 3000);
   app.set('views', config.root + '/app/views');
   app.set('view engine', 'jade');
-  // app.use(express.favicon());
+  app.use(express.favicon());
   // app.use(express.logger('dev'));
   app.use('/upload', upload.fileHandler());
   app.use(express.bodyParser());
