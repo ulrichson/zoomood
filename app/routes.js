90283aac855056371a64fafd477346d5f1c3b9ba
Ulrich Lehner <me@ulrichlehner.com>
2015-11-23 09:16:19 +0100

    Revert "Revert "Improved comment format for acidic""
    This reverts commit 05ada2529e566c0476e45992b48ccd85fe505f38.

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ module.exports = function(app, config, io) {
   var media = require('./controllers/media')(config, io);
   var session = require('./controllers/session')(config);
 
-  // GET index
+  // Index page (canvas)
   app.get('/', media.home);
 
   /**
@@ -30,18 +30,18 @@ module.exports = function(app, config, io) {
    * @api {get} /media/:name Get one media
    * @apiGroup Media
    *
-   * @apiParam {String} name Media name.
+   * @apiParam {String}     name              Media name.
    *
-   * @apiSuccess {String}   name          The name.
-   * @apiSuccess {Number}   x             The x position on canvas.
-   * @apiSuccess {Number}   y             The y position on canvas.
-   * @apiSuccess {Number}   scale         The scale.
-   * @apiSuccess {String}   url           The URL.
-   * @apiSuccess {Object}   x             The x position on canvas.
-   * @apiSuccess {Number}   angle         The angle.
-   * @apiSuccess {String}   type          The type (e.g. 'whiteboard').
-   * @apiSuccess {Number}   order         The order on canvas (layer).
-   * @apiSuccess {Number}   session       The session id.
+   * @apiSuccess {String}   name              The name.
+   * @apiSuccess {Number}   x                 The x position on canvas.
+   * @apiSuccess {Number}   y                 The y position on canvas.
+   * @apiSuccess {Number}   scale             The scale.
+   * @apiSuccess {String}   url               The URL.
+   * @apiSuccess {Object}   x                 The x position on canvas.
+   * @apiSuccess {Number}   angle             The angle.
+   * @apiSuccess {String}   type              The type (e.g. 'whiteboard').
+   * @apiSuccess {Number}   order             The order on canvas (layer).
+   * @apiSuccess {Number}   session           The session id.
    */
   app.get('/media/:name', media.show);
 
@@ -49,10 +49,10 @@ module.exports = function(app, config, io) {
    * @api {delete} /media/:name Delete one media
    * @apiGroup Media
    *
-   * @apiParam {String} name Media name.
+   * @apiParam {String}     name               Media name.
    *
-   * @apiSuccess {String} message Return message.
-   * @apiSuccess {String} media_name Name of deleted media.
+   * @apiSuccess {String}   message            Return message.
+   * @apiSuccess {String}   media_name         Name of deleted media.
    */
   app.delete('/media/:name', media.deleteOne);
 
@@ -69,11 +69,11 @@ module.exports = function(app, config, io) {
    *       "order": 2
    *     }
    *
-   * @apiParam {String} name Media name.
-   * @apiParam {Number} scale The scale factor.
-   * @apiParam {Number} x The x position on canvas.
-   * @apiParam {Number} y The y position on canvas.
-   * @apiParam {Number} order The order on canvas (layer).
+   * @apiParam {String}     name                Media name.
+   * @apiParam {Number}     scale               The scale factor.
+   * @apiParam {Number}     x                   The x position on canvas.
+   * @apiParam {Number}     y                   The y position on canvas.
+   * @apiParam {Number}     order               The order on canvas (layer).
    */
   app.put('/media/:name', media.update);
 
@@ -81,10 +81,10 @@ module.exports = function(app, config, io) {
    * @api {post} /media Upload new media
    * @apiGroup Media
    *
-   * @apiParam {String} image_base64 Base64 encoded image in png or jpg.
+   * @apiParam {String}     image_base64        Base64 encoded image in png or jpg.
    *
-   * @apiSuccess {String} message Return message.
-   * @apiSuccess {String} media Uploaded media object.
+   * @apiSuccess {String}   message             Return message.
+   * @apiSuccess {String}   media               Uploaded media object.
    */
   app.post('/media', media.post);
 
@@ -92,10 +92,10 @@ module.exports = function(app, config, io) {
    * @api {post} /session Create new session
    * @apiGroup Session
    *
-   * @apiParam {String} [name] Name for session.
+   * @apiParam {String}     [name]              Name for session.
    *
-   * @apiSuccess {String} message Return message.
-   * @apiSuccess {String} session Uploaded session object.
+   * @apiSuccess {String}   message             Return message.
+   * @apiSuccess {String}   session             Uploaded session object.
    */
   app.post('/session', session.post);
 
@@ -115,7 +115,7 @@ module.exports = function(app, config, io) {
    * @api {delete} /session Delete all sessions
    * @apiGroup Session
    *
-   * @apiSuccess {String} message Return message.
+   * @apiSuccess {String}   message             Return message.
    */
   app.delete('/session', session.deleteAll);
 
@@ -125,7 +125,7 @@ module.exports = function(app, config, io) {
    *
    * @apiParam {String} id Session id.
    *
-   * @apiSuccess {String} message Return message.
+   * @apiSuccess {String}   message             Return message.
    */
   app.delete('/session/:id', session.deleteOne);
 
@@ -141,10 +141,10 @@ module.exports = function(app, config, io) {
    * @api {put} /session/:id Set active session
    * @apiGroup Session
    *
-   * @apiParam {String} id Session id.
+   * @apiParam {String}     id                  Session id that will be activated.
    *
-   * @apiSuccess {String} message Return message.
-   * @apiSuccess {String} session Session object.
+   * @apiSuccess {String}   message             Return message.
+   * @apiSuccess {String}   session             Session object.
    */
   app.put('/session/active/:id', session.setActive);
 
@@ -152,9 +152,9 @@ module.exports = function(app, config, io) {
    * @api {post} /session/canvas Upload session canvas to active canvas
    * @apiGroup Session
    *
-   * @apiParam {String} image_base64 Base64 encoded image in png or jpg.
+   * @apiParam {String}     image_base64        Base64 encoded image in png or jpg.
    *
-   * @apiSuccess {String} message Return message.
+   * @apiSuccess {String}   message             Return message.
    */
   app.post('/session/canvas', session.postCanvas);
 }
